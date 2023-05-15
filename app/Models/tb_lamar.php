6a575857d590f19403ce2e22f586ec9752caae43
iRzellA <danyahmadadfn@gmail.com>
2023-05-15 11:39:05 +0700

    Update

---
+++ app/Models/tb_lamar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tb_lamar extends Model
+{
+    use HasFactory;
+}
